96cbde420d3e489fc011c8801dbe3bb4e8ad9ced
leitek1 <leitek1@yandex.ru>
2023-06-01 22:46:14 +0500

    HW1 is completed

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"fmt"
+
+	"golang.org/x/example/stringutil"
+)
+
+const hello string = "Hello, OTUS!"
+
 func main() {
-	// Place your code here.
+	fmt.Print(stringutil.Reverse(hello))
 }
